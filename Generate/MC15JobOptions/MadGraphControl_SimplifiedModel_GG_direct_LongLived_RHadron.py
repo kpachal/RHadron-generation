2a9dbf523027ed159cfc9714d71ea47e73747c50
kpachal <katherine.pachal@cern.ch>
2019-01-25 06:22:04 -0800

    shortened names

--- Generate/MC15JobOptions/MadGraphControl_SimplifiedModel_GG_direct_LongLived_RHadron.py
+++ Generate/MC15JobOptions/MadGraphControl_SimplifiedModel_GG_direct_LongLived_RHadron.py
@@ -5,7 +5,7 @@ infoStrings = runArgs.jobConfig[0].split("/")[-1].split(".")[2].split("_")
 masses['1000021'] = float(infoStrings[5])
 masses['1000022'] = float(infoStrings[6].split('.py')[0])
 lifetime = float(infoStrings[7].replace("ns","").split('.py')[0].replace('p','.') ) #in ns
-spectrum = int(infoStrings[8].split('.py')[0].replace('mass',''))
+spectrum = int(infoStrings[8].split('.py')[0].replace('sp',''))
 if '_gl' in runArgs.jobConfig[0]:
     gluinoballProb = float(infoStrings[-1].split('.')[0].replace("gl",""))*0.01
 else:
