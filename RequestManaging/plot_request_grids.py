b22010765f0ef275e2b092aeda4fb186c998441e
Katherine Pachal <katherine.pachal@cern.ch>
2019-03-28 11:43:17 +0100

    first pass at summing. not yet validated

--- RequestManaging/plot_request_grids.py
+++ RequestManaging/plot_request_grids.py
@@ -25,9 +25,19 @@ all_lifetimes = list(set(request_DV.keys() + request_dEdx.keys() + request_stopp
 # Make a plot per lifetime.
 # Also, determine maximum number of events being requested at each point.
 # If max number comes from stopped particle, shout....
+
+# These will store sizes of event requests at each point,
+# assuming analyses share wherever possible.
+n_EVNT = {}
+n_FullSim = {}
+n_specialSim = {}
+dict_list = [n_EVNT,n_FullSim,n_specialSim]
+
 for lifetime in all_lifetimes :
 
   print "Starting lifetime",lifetime
+  for mydict in dict_list :
+    mydict[lifetime] = {}
 
   graphs = []
 
@@ -45,11 +55,42 @@ for lifetime in all_lifetimes :
 
     for mGluino in mass_grid.keys() :
 
+      for mydict in dict_list :
+        if not mGluino in mydict[lifetime].keys() :
+          mydict[lifetime][mGluino] = {}
+
       for mNeutrino in mass_grid[mGluino].keys() :
+
+        # Fill graphs
 
         index = thisgraph.GetN()
         thisgraph.SetPoint(index,mGluino, mNeutrino)
         thisgraph.SetPointError(index,0,mass_grid[mGluino][mNeutrino])
+
+        # Store or update number of events
+
+        # Want all of them for EVNT
+        if not mNeutrino in n_EVNT[lifetime][mGluino].keys() :
+          n_EVNT[lifetime][mGluino] = {mNeutrino : mass_grid[mGluino][mNeutrino]}  
+        else :
+          if mass_grid[mGluino][mNeutrino] > n_EVNT[lifetime][mGluino][mNeutrino] :
+            n_EVNT[lifetime][mGluino][mNeutrino] = mass_grid[mGluino][mNeutrino]
+
+        # Want non-stopped-particle for Fullsim
+        if not "Stopped" in request :
+          if not mNeutrino in n_FullSim[lifetime][mGluino].keys() :
+            n_FullSim[lifetime][mGluino] = {mNeutrino : mass_grid[mGluino][mNeutrino]}  
+          else :
+            if mass_grid[mGluino][mNeutrino] > n_FullSim[lifetime][mGluino][mNeutrino] :
+              n_FullSim[lifetime][mGluino][mNeutrino] = mass_grid[mGluino][mNeutrino]
+
+        # Want stopped particle only for special reconstruction
+        else :
+          if not mNeutrino in n_specialSim[lifetime][mGluino].keys() :
+            n_specialSim[lifetime][mGluino] = {mNeutrino : mass_grid[mGluino][mNeutrino]}  
+          else :
+            if mass_grid[mGluino][mNeutrino] > n_specialSim[lifetime][mGluino][mNeutrino] :
+              n_specialSim[lifetime][mGluino][mNeutrino] = mass_grid[mGluino][mNeutrino]
 
     print "Adding graph for request",request
     graphs.append(thisgraph)
@@ -59,4 +100,39 @@ for lifetime in all_lifetimes :
   else :
     myPainter.drawSignalGrid(graphs,"plots/grid_{0}".format(lifetime),request_list,"m_{G} [GeV]","automatic","automatic","m_{#chi} [GeV]","automatic","automatic",addDiagonal=False)
 
+# Now summarise number of events at each lifetime. 
+# Print things to check them by eye.
+total_EVNT = 0
+total_FullSim = 0
+total_SpecialReco = 0
+for lifetime in all_lifetimes :
 
+  print "For lifetime",lifetime
+  print "mGluino, mNeutralino, number of EVNT:"
+
+  for mGluino in sorted(n_EVNT[lifetime].keys()) :
+    for mNeutrino in sorted(n_EVNT[lifetime][mGluino].keys()) :
+      thisEVNT = n_EVNT[lifetime][mGluino][mNeutrino]
+      print "\t",mGluino,"\t",mNeutrino,"\t",thisEVNT
+      total_EVNT = total_EVNT+thisEVNT
+
+  print "number of FullSim:"
+
+  for mGluino in sorted(n_FullSim[lifetime].keys()) :
+    for mNeutrino in sorted(n_FullSim[lifetime][mGluino].keys()) :
+      thisFS = n_FullSim[lifetime][mGluino][mNeutrino]
+      print "\t",mGluino,"\t",mNeutrino,"\t",thisFS
+      total_FullSim = total_FullSim+thisFS
+
+  print "Number of special reco for Stopped Particle:"
+
+  for mGluino in sorted(n_specialSim[lifetime].keys()) :
+    for mNeutrino in sorted(n_specialSim[lifetime][mGluino].keys()) :
+      this_special = n_specialSim[lifetime][mGluino][mNeutrino]
+      print "\t",mGluino,"\t",mNeutrino,"\t",this_special
+      total_SpecialReco = total_SpecialReco+this_special
+
+print "\nTotals:"
+print "\tEVNT:",total_EVNT,"=",round(float(total_EVNT)/1000000.0,2),"million"
+print "\tFullSim:",total_FullSim,"=",round(float(total_FullSim)/1000000.0,2),"million"
+print "\tSpecial for Stopped Particle:",total_SpecialReco,"=",round(float(total_SpecialReco)/1000000.0,2),"million"
